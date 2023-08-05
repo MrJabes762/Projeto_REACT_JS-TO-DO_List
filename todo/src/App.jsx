bb7f382aa57b6566e291af09f5921f8955a1017d
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-08-05 01:43:48 -0300

    Desenvolvimento (#3)
    * Limpeza dos Arquivos
    
    * Configurações inciais concluidas
    
    * app

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,7 +1,46 @@
 import { useState } from 'react'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para Academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ])
+
+  return (
+  <div className="app">
+    <h1>lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              <p> {todo.text}</p>
+              <p className="category">{todo.category}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+  </div>
+);
+
   return 0;
 }
 
-export default App
+
+export default App;
+}
