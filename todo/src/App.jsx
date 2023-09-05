6084c3b93dcd8ecdee67af6995c9dff93a2faba7
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-09-04 23:47:17 -0300

    Construção inicial do app

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,7 +1,43 @@
 import { useState } from 'react'
 
 function App() {
-  return  <div>Olá Mundo</div>
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema ",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia ",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React ",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+  return <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p className='category'>({todo.category})</p>
+          </div>
+          <div>
+            <button>Completar</button>
+            <button>x</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>;
 }
 
 export default App
