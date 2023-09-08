88d3e3c09dedc47ab8653d288f172374f8f6240f
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-09-07 21:41:20 -0300

    Desenvolvimento (#7)
    * Construção inicial do app
    
    * Estilização inicial da lista de tarefas
    
    * Estilização dos botões e criaçao do form
    
    * Concertando Todo Form
    
    * Manipulação dos dados
    
    * Ajustes add/rmvr tarefas e finalização de layout
    
    * Finalizando a função conplete
    
    * Implementado a função de pesquisa de tarefas
    
    * Layout final da Lista de Tarefas
    
    * Conclusão da Parte Lógica do Filtro de Atividades
    
    * Finalizando o projeto com Adição de Asc e Desc

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,8 +1,97 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Todo from "./components/Todo";
+import "./App.css";
+import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 function App() {
-  return  <div>Olá Mundo</div>
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+  const [search, setSearch] = useState(""); // Correção: Separe o estado search de setSearch
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          isCompleted: !todo.isCompleted,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} /> {/* Correção: Passar search e setSearch como props */}
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
+      <div className="todo-list">
+        {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter == "Completed"
+                ? todo.isCompleted
+                : !todo.isCompleted)
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text))
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo} />
+          ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
 }
 
-export default App
-
+export default App;
