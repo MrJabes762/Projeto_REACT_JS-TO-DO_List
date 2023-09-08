5e7c1eb76730f71de2b3fdeea8a8704b74b35822
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-09-07 21:38:27 -0300

    Finalizando o projeto com Adição de Asc e Desc

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -65,7 +65,7 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} /> {/* Correção: Passar search e setSearch como props */}
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
           .filter((todo) =>
@@ -77,6 +77,10 @@ function App() {
           .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
           )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text))
           .map((todo) => (
             <Todo
               key={todo.id}
--- todo/src/components/Filter.jsx
+++ todo/src/components/Filter.jsx
@@ -1,4 +1,4 @@
-const Filter = ({filter, setFilter}) => {
+const Filter = ({filter, setFilter, setSort}) => {
   return (
     <div className="filter">
         <h2>Filtrar</h2>
@@ -13,8 +13,8 @@ const Filter = ({filter, setFilter}) => {
             </div>
             <div>
                 <p>Ordem Alfabética</p>
-                <button>Acs</button>
-                <button>Desc</button>
+                <button onClick={() => setSort ("Asc")} >Acs</button>
+                <button onClick={() => setSort ("Desc")}>Desc</button>
             </div>
         </div>
     </div>
